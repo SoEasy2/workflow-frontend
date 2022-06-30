576315feb6c77e7286f2fc8280f829ff6b496c38
SoEasy2 <soeasy24@mail.ru>
2022-06-30 23:54:38 +0300

    Test linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-const App = () => {
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
   return (
     <div className='App'>
-      Init application
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   )
 }
 
-export  { App };
+export default App
