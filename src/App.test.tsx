7a3128fdc8e10c0d529a29218063098bbedc942e
RomanStass <roman.stasianok@cbitech.ae>
2022-09-21 15:04:58 +0300

    Added dev

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
-import { TestError } from './pages';
+import { TestError } from './pages'
 
 test('renders learn react link', () => {
   render(<TestError />)
   const linkElement = screen.getByText(/TEST ERROR/i)
-  expect(linkElement).toBeInTheDocument()
+  expect(linkElement).toBeInTheDocument();
 })
